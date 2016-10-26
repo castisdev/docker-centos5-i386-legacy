58b2bdaf9b76bdb6d674408038db2ea80e1eceec
sasgas <sasgas@castis.com>
2016-10-26 14:28:01 +0900

    docker build 시에 cryptopp 빌드에서 -m64 선택되는 것 수정

--- install_cryptopp563.sh
+++ install_cryptopp563.sh
@@ -3,7 +3,7 @@ cd ~
 wget http://www.cryptopp.com/cryptopp563.zip
 unzip cryptopp563.zip -d cryptopp
 cd cryptopp
-sed -e s/march=native/march=i686/g GNUmakefile > tmp_make
+sed -e s/march=native/march=i686/g GNUmakefile | sed -e s/-m64/-m32/g > tmp_make
 mv tmp_make GNUmakefile
 make static
 make install
